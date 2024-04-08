7b22c1516519874950c49908d0ed7a7f1f117899
suNR4Y0 <160470385+suNR4Y0@users.noreply.github.com>
2024-04-08 08:41:50 +0300

    Create practika.js

---
+++ practika.js
@@ -0,0 +1,21 @@
+// Функция для нахождения площади квадрата
+function squareArea(side) {
+    return side * side;
+}
+
+// Функция для нахождения периметра квадрата
+function squarePerimeter(side) {
+    return 4 * side;
+}
+
+// Пример использования функций
+const sideLength = 5;
+const area = squareArea(sideLength);
+const perimeter = squarePerimeter(sideLength);
+
+console.log(`Площадь квадрата: ${area}`);
+console.log(`Периметр квадрата: ${perimeter}`);
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
