f0092bb7be36851cc34c2c43366538d269940b7b
suNR4Y0 <160470385+suNR4Y0@users.noreply.github.com>
2024-04-08 08:47:06 +0300

    Update Практика JavaScript.js

--- Практика JavaScript.js
+++ Практика JavaScript.js
@@ -17,10 +17,6 @@ const perimeter = squarePerimeter(sideLength);
 
 console.log(`Площадь квадрата: ${area}`);
 console.log(`Периметр квадрата: ${perimeter}`);
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
-console.log("Try programiz.pro");
 ////////////////////////1.2
 
 // Функция для вычисления площади прямоугольника
